3ee775bf7af3c4bd00e8636f5a5b506f1228f5fa
Sandesh Shahi <devsanbid@gmail.com>
2024-03-01 11:32:08 +0545

    added new binding

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,19 +10,28 @@ keymap("i", "jk", "<ESC>")
 keymap("i", "JK", "<ESC>")
 keymap("i", "kj", "<ESC>")
 keymap("i", "jj", "<ESC>")
-
+keymap("i", "Jk", "<ESC>")
 
 keymap("n", "<leader>jm", ":NodeAction<cr>")
 keymap("n", "<leader>jj", ":TSJToggle<cr>")
 keymap("x", "p", [["_dP]])
 
+-- keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
+
 keymap({ "n", "x" }, "j", "gj", opts)
 keymap({ "n", "x" }, "k", "gk", opts)
 
 keymap("n", "*", "*zz", opts)
-keymap("n", "#", "#zz", opts)
+keymap("n", "<C-z>", "*zz", opts)
+keymap("n", "<C-S-z>", "#zz", opts)
 keymap("n", "g*", "g*zz", opts)
 keymap("n", "g#", "g#z", opts)
+
+-- take screenshot
+keymap({ "n", "v" }, "<leader>ss", ":Silicon<cr>")
 
 -- buffer
 keymap("n", "<leader>c", ":bdelete<cr>", opts)
@@ -47,7 +56,7 @@ keymap("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 --telescope
 keymap("n", "<leader>ff", ":Telescope find_files<cr>", opts)
 keymap("n", "<leader><leader>", ":Telescope find_files<cr>", opts)
-keymap("n", "<leader>fg", ":Telescope live_grep<cr>", opts)
+keymap("n", "<leader>fw", ":Telescope live_grep<cr>", opts)
 keymap("n", "<leader>fH", ":Telescope help_tags<cr>", opts)
 keymap("n", "<leader>fr", ":Telescope oldfiles<cr>", opts)
 keymap("n", "<leader>fh", ":Telescope highlights<cr>", opts)
@@ -74,7 +83,7 @@ keymap("n", "<leader>sm", ":e ~/.config/nv/lua/core/init.lua<cr>", opts)
 keymap("n", "<leader>sk", ":e ~/.config/nv/lua/core/keymaps.lua<cr>", opts)
 keymap("n", "<leader>so", ":e ~/.config/nv/lua/core/options.lua<cr>", opts)
 keymap("n", "<leader>sa", ":e ~/.config/nv/lua/core/autocmds.lua<cr>", opts)
-keymap("n", "<leader>sh", ":e ~/.config/nv/lua/core/highlights.lua<cr>", opts)
+keymap("n", "<leader>sh", ":e ~/.config/nv/lua/core/highlight.lua<cr>", opts)
 keymap("n", "<leader>sp", ":e ~/.config/nv/lua/plugins/init.lua<cr>", opts)
 
 vim.cmd("vnoremap im aBoV")
