ce9c97665cfccb60f3203983b10c2e7961ad4295
devsanbid <devsanbid@gmail.com>
2024-03-07 10:47:40 +0000

    remove default image

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
-
 require("core")
 require("lazys")
 require("config.lsp")
